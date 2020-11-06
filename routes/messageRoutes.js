bb6b84d839007ba2c82fa9b2ab646e102bbd6467
sujon13 <arifurrahmansujon27@gmail.com>
2020-11-07 01:03:28 +0600

    FIxed getMessage api

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -6,15 +6,22 @@ const Message = require('../models/Message');
 router.get('/', async (req, res) => {
     const from = req.query.from;
     const to = req.query.to;
+    const skip = parseInt(req.query.skip);
 
     try {
         const messages = await Message.find(
             {
                 from: { $in: [from, to] },
                 to: { $in: [from, to] }
-            }
+            },
+            null,
+            { sort: '-time', skip: skip, limit: 30}
         );
-        
+        console.log('Before sort: ', messages);
+        messages.sort(function(a, b) {
+            return a.time.getTime() - b.time.getTime();
+        });
+        console.log('After sort: ', messages);
         res.status(200).send(messages);
     } catch (error) {
         res.send([]);
